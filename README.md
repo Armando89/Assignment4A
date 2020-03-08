7a73377ff7181c74e8269a7e49e4490afbacebcf
Armando89 <43739205+Armando89@users.noreply.github.com>
2020-03-08 15:53:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment4A
+ Using do-while loop
---
+++ src/assignment4a/Assignment4A.java
@@ -0,0 +1,19 @@
+//Armando Galan-Cedeño  Assignment4A
+//CS121 Spring 2020
+//March 3, 2020
+//Purpose: Create a payroll calculator program using a "do..while" loop. 
+
+package assignment4a;
+                               
+import java.util.Scanner;       //initiating class Scanner
+                                                           
+public class Assignment4A {
+                            
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);     //creating Scanner to obtain input
+        
+        
+        
+    }
+}
